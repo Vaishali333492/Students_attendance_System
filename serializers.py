beb8699d7ecb93ce16443672cdb58e45131e0c7c
Vaishali333492 <131801784+Vaishali333492@users.noreply.github.com>
2026-08-27 20:02:45 +0530

    /qr_attendance/accounts

--- serializers.py
+++ serializers.py
@@ -1,18 +1,30 @@
 from rest_framework import serializers
-
-from .models import ClassRoom, AttendanceSession
-
-
-class ClassRoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ClassRoom
-        fields = ('id', 'name', 'code', 'description')
+from .models import User
 
 
-class AttendanceSessionSerializer(serializers.ModelSerializer):
-    classroom = ClassRoomSerializer(read_only=True)
-    teacher = serializers.CharField(source='created_by.user.username', read_only=True)
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = AttendanceSession
-        fields = ('token', 'classroom', 'teacher', 'start_time', 'expires_at', 'active')
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'phone',
+            'role',
+            'password',
+        )
+
+    def create(self, validated_data):
+
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            phone=validated_data.get('phone'),
+            role=validated_data['role'],
+            password=validated_data['password']
+        )
+
+        return user
