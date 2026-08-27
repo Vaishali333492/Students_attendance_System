809bcc7f8b2227d8f2a5128609c7154ab9d4453f
Vaishali333492 <131801784+Vaishali333492@users.noreply.github.com>
2026-08-27 15:54:31 +0530

    /attendance

--- urls.py
+++ urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path('register/', views.register),
-
-    path('login/', views.login),
-
+    path('classrooms/', views.classroom_list, name='classroom-list'),
+    path('session/', views.generate_qr_token, name='generate-qr-token'),
+    path('scan/', views.scan_attendance, name='scan-attendance'),
 ]
